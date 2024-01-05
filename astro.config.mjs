37e075dd0a2656288d48a936a548a504980dbfa2
Dizi <58800976+DiziASP@users.noreply.github.com>
2024-01-06 04:54:45 +0700

    feat: anjay

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,15 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
-
 import tailwind from "@astrojs/tailwind";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "http://localhost:4321",
-  integrations: [
-    react(),
-    tailwind({
-      applyBaseStyles: false,
-    }),
-  ],
+  site: "https://dizi-assyafadi.vercel.app",
+  integrations: [react(), tailwind({
+    applyBaseStyles: false
+  })],
+  output: "server",
+  adapter: vercel()
 });
