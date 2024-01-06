bf3840f34810162d2cf18f306144f7669a447fba
Dizi <58800976+DiziASP@users.noreply.github.com>
2024-01-06 14:33:40 +0700

    feat: ganti webp

--- src/lib/site.config.ts
+++ src/lib/site.config.ts
@@ -46,7 +46,7 @@ export const project = [
     title: "GasBall: Software Enginerring Final Task",
     description:
       "For Final Task of Software Engineering Course, I Developed a Full-stack Field Management Website using NextJS and Supabase. This is the most complex project I've ever developed since I have to build numerous use case for football field management. I've also integrate a simple CI/CD Pipeline using Github Actions with simple Test Unit using Jest. The website itself is deployed using Vercel and the database is deployed on Supabase Server connected using REST API.",
-    image: "/images/gasball.png",
+    image: "/images/gasball.webp",
     year: "2023",
     tech: ["NextJS", "Supabase", "TailwindCSS", "Docker"],
     github: "https://github.com/DiziASP/gasball-deploy",
@@ -56,7 +56,7 @@ export const project = [
     title: "OnlyTrade: Microservice Project",
     description:
       "A Fullstack Web Application built using Codeigniter 4 and Tailwind CSS. This project is a part of my Integration System Technology Course's Final Task. This project was very demanding since I need to build two separate Application, one for handling the Management System (Single Service) and the E-Commerce Website (Monolithic) both built using Codeigniter 4. The project was initially deployed using Railway but has since been taken down due to insufficient credits",
-    image: "/images/onlytrade.png",
+    image: "/images/onlytrade.webp",
     year: "2023",
     tech: ["Codeigniter 4", "MariaDB", "TailwindCSS", "Docker"],
     github: "https://github.com/DiziASP/OnlyTrade-Single-Service",
@@ -65,7 +65,7 @@ export const project = [
     title: "API Performance Dashboard",
     description:
       "This project is the final task for Analysis and Design of System Performance Course. I developed a Monitoring Dashboard for API Performance using Prometheus, Grafana, and K6 using Docker. The main challenge of this project is to scrap the Resource Usage (Docker) hence I have to use NodeExporter for the NodeJS Backend and Vanilla Prometheus for the Docker CPU and Memory Usage",
-    image: "/images/api.png",
+    image: "/images/api.webp",
     year: "2023",
     tech: ["Prometheus", "Grafana", "K6", "Docker", "Express"],
     github: "https://github.com/DiziASP/tubes-apks-2023",
@@ -74,7 +74,7 @@ export const project = [
     title: "Arkavidia 8.0 Website",
     description:
       "This project is the official website for Arkavidia 8.0, an annual IT Competition held by HMIF ITB. I developed the website using NextJS with TypeScript and TailwindCSS. I also developed the website's admin panel using ReactJS and TailwindCSS. There is no picture provided since the event is already completed and the website has already been taken down",
-    image: "/images/arkavidia.png",
+    image: "https://placehold.co/314x192/webp?text=No%20Image",
     tech : ["NextJS", "TailwindCSS"],
     year: "2022",
     github: "https://github.com/arkavidia-hmif/arkavidia-frontend-8.0"
@@ -83,7 +83,7 @@ export const project = [
     title: "Rakata Website",
     description:
       "This project was built for Rakata ITB (ITB's eco-friendly and renewable energy vehicles extracurricular) Website. I developed the admin form using TinyMCE on top of ReactJS. It was my first time handling a professional project thanks to my senior whom invited me to help him on this project",
-    image: "/images/rakata.png",
+    image: "/images/rakata.webp",
     year: "2022",
     tech: ["ReactJS", "Express", "TinyMCE"],
     demo: "https://rakata.vercel.app/",
